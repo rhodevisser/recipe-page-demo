68ae616f8cd1bb0490a426006d40aa50c3fbd015
kapufskipauw <rosarhode@gmail.com>
2025-12-22 22:55:18 +0100

    add: Arrays with ingredients
    for each recipe containing its own ingredients

--- index.php
+++ index.php
@@ -8,6 +8,31 @@ $totalPreparationTime = [
     "5 minutes cooking"
 ];
 
+$ingredients = [
+    [
+        'recipe' => 'oats',
+        'base' => '6 spoons dry oats',
+        'secondBase' => '400ml milk',
+        'butter' => 'salted cream butter',
+        'spice' => 'teaspoon cinnamon, and pinch of salt and cardemon'   
+    ],
+    [
+        'recipe' => 'banana bread',
+        'base' => 'one banana',
+        'secondBase' => 'one egg',
+        'butter' => 'salted cream butter',
+        'spice' => 'pinch of salt' 
+    ],
+    [
+        'recipe' => 'balkan breakfast',
+        'base' => 'random veggies and soured cucumber',
+        'secondBase' => 'bread and cheese',
+        'butter' => 'olive oil',
+        'spice' => 'salt and pepper'   
+    ]
+];
+
+
 
 
 require "view.index.php";
